44e5716fd1e3230e665811836cb5cff2f92d0eb5
Reno <reno.davids@sovtech.com>
2023-06-02 15:39:49 +0200

    feat: Final styling to chatbox

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -8,13 +8,19 @@ import {
   VStack,
 } from "@chakra-ui/react";
 
+const buttonsText = [
+    "Emergency Help", 
+    "Am I having a panic attack ?", 
+    "How do I do CPR", 
+    "Closest Hospital"
+]
+
 export default function chatPage() {
   return (
-    <Center h="100vh">
-
+    <Center h="100vh" bg="##F6FEFD">
       <Box
         w="90%"
-        bg="##FFFCF5"
+        bg="#FFFCF5"
         shadow="xl"
         border="1px"
         borderColor="black"
@@ -23,9 +29,9 @@ export default function chatPage() {
         paddingBottom="5vh"
         position="relative"
       >
-        <VStack alignContent="center" spacing={5}>
-          <Text as="b" fontSize="3xl">
-            Hi there!
+        <VStack alignContent="center" spacing={5} paddingX={[0, "15vw"]} paddingTop={["3vh", "0"]}>
+          <Text as="b" fontSize="3xl" textAlign="center">
+          Hi there! Get started by chatting to Ozzy
           </Text>
           <Text textAlign="center">
             Or select one of the options for immediate assistance.
@@ -34,42 +40,19 @@ export default function chatPage() {
             templateColumns={["repeat(2, 1fr)", "repeat(4, 1fr)"]}
             gap={[3, 5]}
           >
-            <Button
-              whiteSpace="normal"
-              bg="#F79009"
-              textColor="white"
-              shadow="lg"
-              padding={6}
-            >
-              Emergency Help
-            </Button>
-            <Button
-              whiteSpace="normal"
-              bg="#F79009"
-              textColor="white"
-              shadow="lg"
-              padding={6}
-            >
-              Am I having a panic attack?
-            </Button>
-            <Button
-              whiteSpace="normal"
-              bg="#F79009"
-              textColor="white"
-              shadow="lg"
-              padding={6}
-            >
-              How do I do CPR
-            </Button>
-            <Button
-              whiteSpace="normal"
-              bg="#F79009"
-              textColor="white"
-              shadow="lg"
-              padding={6}
-            >
-              Closet Hospital
-            </Button>
+            {
+                buttonsText.map((text, index)=>{
+                    return (
+                        <Button key={index} whiteSpace="normal"
+                        bg="#F79009"
+                        textColor="white"
+                        shadow="lg"
+                        padding={3} height="auto" >
+                            {text}
+                        </Button>
+                    )
+                })
+            }
           </Grid>
           <Input
             placeholder="You can speak or type to talk to Ozzy..."
