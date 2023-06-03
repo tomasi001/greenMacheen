0266ef05f4f2d94c33e78f84b1b9700025bda70e
Reno <reno.davids@sovtech.com>
2023-06-03 02:22:45 +0200

    feat: Adaptive textarea. Static Ozzy

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -16,7 +16,8 @@ import Lotty from "~/components/Lotty";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
-import { ClaudeService } from "~/@core/services/claude/cloude-service";
+import { ClaudeService } from "~/@core/services/claude/cloude-service"; 
+import Image from "next/image";
 
 
 export default function ChatPage() {
@@ -24,22 +25,22 @@ export default function ChatPage() {
   const [prompt, setPrompt] = useState("");
   const [promptArray, setPromptArray] = useState([])
   const [responseArray, setResponseArray] = useState([])
+  const textareaRef = useRef()
 
   let messageArray = promptArray.flatMap((item, index) => [item, responseArray[index]])
 
   const [isLoading, setIsLoading] = useState(false);
   const { transcript, listening } = useSpeechRecognition();
 
-  const fetchResponse = async () => {
-    setIsLoading(true);
-    const result = await completeClaudRequest(transcript);
-    setResponse(result);
-    setIsLoading(false);
-  };
-
   const handleInput = (event) =>{
     setPrompt(event.target.value)
   }
+
+  useEffect(()=>{
+    const textArea = textareaRef.current
+    textArea.style.height = 'auto'
+    textArea.style.height = `${textArea.scrollHeight}px`
+  }, [prompt])
 
   const sendPrompt = async () => {
     setPromptArray([...promptArray, prompt])
@@ -62,12 +63,13 @@ export default function ChatPage() {
 
   return (
     <Center h="100vh" bg="##F6FEFD">
-      <Lotty
+      {/* <Lotty
         bottom="185px"
         right="70px"
         position="absolute"
         transform="scale(0.3)"
-      />
+      /> */}
+      
       <Box
         w={["90%", "80%"]}
         bg="#FFFCF5"
@@ -83,6 +85,7 @@ export default function ChatPage() {
           paddingX={[0, "15vw"]}
           paddingTop={["3vh", "0"]}
         >
+          <Image src="/Ozzy.png" alt="Ozzy" width={100} height={30}/>
           <Text as="b" fontSize="3xl" textAlign="center">
             Hi there! Get started by chatting to Ozzy
           </Text>
@@ -99,8 +102,8 @@ export default function ChatPage() {
             </SimpleGrid>
           </Box>
           <HStack>
-              
-            <Input placeholder="You can speak or type to talk to Ozzy..." bg="white" width="50vh" shadow="xl" onChange={handleInput} value={prompt}/>
+          
+            <Textarea placeholder="You can speak or type to talk to Ozzy..." bg="white" width="50vh" shadow="xl" onChange={handleInput} value={prompt} ref={textareaRef} />
             <Button bg="#F79009" size="md" variant="solid"
                 borderRadius="full"
                 height="45px"
@@ -109,6 +112,7 @@ export default function ChatPage() {
                 shadow="xl"
                 textColor="white"
                 onClick={sendPrompt}
+                isLoading={isLoading}
               >
               <i className="ri-send-plane-2-line"></i>
             </Button>
@@ -130,7 +134,6 @@ export default function ChatPage() {
           </HStack>
         </VStack>
         <Center>
-          
         </Center>
       </Box>
     </Center>
@@ -149,8 +152,6 @@ async function completeClaudRequest(transcript: any) {
       temperature: 0.2,
       top_k: 0.5
     });
-
-    console.log("res", res);
     return res as any;
   } catch (error) {
     console.log("completeClaudRequest >> error ", error);
